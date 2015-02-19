cbbe63745003b3696065f6df2a26da69bb8efe8e
Patrick Toohey <ptoohey@oswego.edu>
2015-02-19 10:51:25 -0500

    fix alignment on message, update personalization message

--- app.js
+++ app.js
@@ -20,8 +20,8 @@
 })();
 
 window.onload = function () {
-    var el = document.getElementById('content');
-    var greeter = new Greeter(el);
-    greeter.start();
+    //var el = document.getElementById('content');
+    //var greeter = new Greeter(el);
+    //greeter.start();
 };
 //# sourceMappingURL=app.js.map
